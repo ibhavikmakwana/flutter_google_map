295c0a63cc73da02375418a1dc6dc9a96440b5a4
bhavik <bhavikmakwana43@gmail.com>
2019-06-16 23:28:32 +0530

    night mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapTypes {
@@ -15,185 +18,235 @@ const List<MapTypes> types = const <MapTypes>[
 ];
 
 void main() {
-  runApp(MyMapApp());
+  runApp(MyApp());
 }
 
-class MyMapApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _MyMapAppState createState() => new _MyMapAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Google Map",
+      home: MyMap(),
+    );
+  }
 }
 
-class _MyMapAppState extends State<MyMapApp> {
-  GoogleMapController mapController;
-  CameraPosition _position;
+class MyMap extends StatefulWidget {
+  @override
+  _MyMapState createState() => _MyMapState();
+}
 
-  GoogleMapOptions _options = GoogleMapOptions(
-    cameraPosition: const CameraPosition(
-      target: LatLng(23.0225, 72.5714),
-      zoom: 11.0,
-    ),
-    trackCameraPosition: true,
-    compassEnabled: true,
+class _MyMapState extends State<MyMap> {
+  GoogleMapController _mapController;
+  MapType _type = MapType.normal;
+  Map<CircleId, Circle> circles = <CircleId, Circle>{};
+
+  static final _options = CameraPosition(
+    target: LatLng(23.0225, 72.5714),
+    zoom: 11,
   );
-  bool _isMoving = false;
+
+  bool _nightMode = false;
 
   @override
   void dispose() {
-    mapController.removeListener(_onMapChanged);
     super.dispose();
   }
 
   ///
   void _select(MapTypes types) {
-    // Causes the app to rebuild with the new _selectedChoice.
+    // Causes the app to rebuild with the selected choice.
     setState(() {
       if (types.title == "Normal") {
-        _mapOptions(MapType.normal);
+        _type = MapType.normal;
       } else if (types.title == "Hybrid") {
-        _mapOptions(MapType.hybrid);
+        _type = MapType.hybrid;
       } else if (types.title == "Satellite") {
-        _mapOptions(MapType.satellite);
+        _type = MapType.satellite;
       } else if (types.title == "Terrain") {
-        _mapOptions(MapType.terrain);
+        _type = MapType.terrain;
       }
     });
   }
 
   ///
   void onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-    mapController.addListener(_onMapChanged);
-    _mapInfo();
-    setState(() {});
-  }
-
-  ///
-  void _onMapChanged() {
     setState(() {
-      _mapInfo();
+      _mapController = controller;
     });
   }
 
-  ///
-  void _mapInfo() {
-    _options = mapController.options;
-    _position = mapController.cameraPosition;
-    _isMoving = mapController.isCameraMoving;
+  Future<String> _getFileData(String path) async {
+    return await rootBundle.loadString(path);
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Google Maps demo'),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.pin_drop),
-              onPressed: () {
-                dropMarker();
-              },
+    return Scaffold(
+      drawer: Drawer(
+        child: mapDrawer(),
+      ),
+      appBar: AppBar(
+        title: const Text('Google Maps demo'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              _nightMode ? Icons.brightness_3 : Icons.brightness_5,
             ),
-            PopupMenuButton<MapTypes>(
-              onSelected: _select,
-              itemBuilder: (BuildContext context) {
-                return types.map((MapTypes types) {
-                  return PopupMenuItem<MapTypes>(
-                    value: types,
-                    child: Text(types.title),
-                  );
-                }).toList();
-              },
-            ),
-          ],
-        ),
-        body: Stack(
-          children: <Widget>[
-            GoogleMap(
-              onMapCreated: onMapCreated,
-              options: _options,
-            ),
-            InfoView(isMoving: _isMoving, position: _position),
-          ],
-        ),
+            onPressed: () {
+              _nightModeToggle();
+            },
+          ),
+          PopupMenuButton<MapTypes>(
+            onSelected: _select,
+            itemBuilder: (BuildContext context) {
+              return types.map((MapTypes types) {
+                return PopupMenuItem<MapTypes>(
+                  value: types,
+                  child: Text(types.title),
+                );
+              }).toList();
+            },
+          ),
+        ],
+      ),
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(
+            initialCameraPosition: _options,
+            onMapCreated: onMapCreated,
+            mapType: _type,
+            circles:  Set<Circle>.of(circles.values),
+            myLocationEnabled: true,
+          ),
+//          InfoView(isMoving: _isMoving, position: _position),
+        ],
       ),
     );
   }
 
-  ///
-  void _mapOptions(MapType type) {
-    mapController.updateMapOptions(
-      GoogleMapOptions(
-        compassEnabled: true,
-        trackCameraPosition: true,
-        mapType: type,
-        cameraPosition: const CameraPosition(
-          target: LatLng(23.0225, 72.5714),
-          zoom: 11.0,
-        ),
-      ),
+  void _nightModeToggle() {
+    if (_nightMode) {
+      setState(() {
+        _nightMode = false;
+        _mapController.setMapStyle(null);
+      });
+    } else {
+      _getFileData('asset/night_mode.json').then((style) {
+        setState(() {
+          _nightMode = true;
+          _mapController.setMapStyle(style);
+        });
+      });
+    }
+  }
+
+  mapDrawer() {
+    return ListView(
+      children: <Widget>[
+        _createDrawerListTile("Add Marker", addMarker),
+        _createDrawerListTile("Add Circle", addCircle),
+        _createDrawerListTile("Add Polyline", addPolyline),
+      ],
     );
   }
+
+  _createDrawerListTile(String listTitle, VoidCallback callback) {
+    return ListTile(
+      title: Text(listTitle),
+      onTap: callback,
+    );
+  }
+
+  void addMarker() {
+//    _mapController.clearMarkers();
+//    _mapController.addMarker(
+//      MarkerOptions(
+//        position: LatLng(23.0225, 72.5714),
+//        draggable: false,
+//        infoWindowText: InfoWindowText("Ahmedabad,", "India"),
+//        consumeTapEvents: true,
+//        icon: BitmapDescriptor.defaultMarker,
+//      ),
+//    );
+  }
+
+  void addCircle() {
+    final Circle circle = Circle(
+      circleId: CircleId("1"),
+      consumeTapEvents: true,
+      strokeColor: Colors.orange,
+      fillColor: Colors.green,
+      strokeWidth: 5,
+      radius: 50000,
+      onTap: () {},
+    );
+
+    setState(() {
+      circles[CircleId("1")] = circle;
+    });
+  }
+
+  void addPolyline() {}
 
   ///Drop marker at the Ahmedabad
-  void dropMarker() {
-    mapController.clearMarkers();
-    mapController.addMarker(
-      MarkerOptions(
-        position: LatLng(23.0225, 72.5714),
-        draggable: false,
-        infoWindowText: InfoWindowText("Ahmedabad,", "India"),
-        consumeTapEvents: true,
-        icon: BitmapDescriptor.defaultMarker,
-      ),
-    );
-  }
+//  void dropMarker() {
+//    mapController.clearMarkers();
+//    mapController.addMarker(
+//      MarkerOptions(
+//        position: LatLng(23.0225, 72.5714),
+//        draggable: false,
+//        infoWindowText: InfoWindowText("Ahmedabad,", "India"),
+//        consumeTapEvents: true,
+//        icon: BitmapDescriptor.defaultMarker,
+//      ),
+//    );
+//  }
 }
 
-class InfoView extends StatelessWidget {
-  const InfoView({
-    Key key,
-    @required bool isMoving,
-    @required CameraPosition position,
-  })  : _isMoving = isMoving,
-        _position = position,
-        super(key: key);
-
-  final bool _isMoving;
-  final CameraPosition _position;
-
-  @override
-  Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.bottomRight,
-      child: Container(
-        width: double.infinity,
-        margin: EdgeInsets.all(8.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8.0),
-          color: Colors.white,
-        ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                _isMoving ? "Map is moving" : "Map is Idle",
-                style: TextStyle(fontSize: 24.0),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                _position != null ? _position.target.toString() : "",
-                style: TextStyle(fontSize: 24.0),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+//class InfoView extends StatelessWidget {
+//  const InfoView({
+//    Key key,
+//    @required bool isMoving,
+//    @required CameraPosition position,
+//  })  : _isMoving = isMoving,
+//        _position = position,
+//        super(key: key);
+//
+//  final bool _isMoving;
+//  final CameraPosition _position;
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Align(
+//      alignment: Alignment.bottomRight,
+//      child: Container(
+//        width: double.infinity,
+//        margin: EdgeInsets.all(8.0),
+//        decoration: BoxDecoration(
+//          borderRadius: BorderRadius.circular(8.0),
+//          color: Colors.white,
+//        ),
+//        child: Column(
+//          mainAxisSize: MainAxisSize.min,
+//          children: <Widget>[
+//            Padding(
+//              padding: const EdgeInsets.all(8.0),
+//              child: Text(
+//                _isMoving ? "Map is moving" : "Map is Idle",
+//                style: TextStyle(fontSize: 24.0),
+//              ),
+//            ),
+//            Padding(
+//              padding: const EdgeInsets.all(8.0),
+//              child: Text(
+//                _position != null ? _position.target.toString() : "",
+//                style: TextStyle(fontSize: 24.0),
+//              ),
+//            ),
+//          ],
+//        ),
+//      ),
+//    );
+//  }
+//}
